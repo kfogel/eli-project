00bfafabe7e87e0b9766f801e168f9c136761209
Karl Fogel <kfogel@red-bean.com>
2024-04-24 16:02:42 -0500

    Add tiny example program for playing sound files

---
+++ playthem.py
@@ -0,0 +1,12 @@
+import time
+import os
+import playsound
+
+for i in range(1, 22):
+    soundfile = f"click{i}.mp3"
+    if os.path.exists(soundfile):
+        print(f"Playing {soundfile}...")
+        playsound.playsound(soundfile)
+        print("Done.")
+        print("")
+    time.sleep(1)
